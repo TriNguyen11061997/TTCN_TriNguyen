5a900600af3fed42ed7990636939edb4472012d2
ADMIN\Trí Nguyễn <ntntri.it1106@gmail.com>
2018-11-21 15:19:58 +0700

    Thành óc

---
+++ src/java/Info/ARCustomersInfo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Info;
+
+/**
+ *
+ * @author Trí Nguyễn
+ */
+public class ARCustomersInfo {
+
+    private String ARCustomerID;
+    private String AAStatus;
+    private String AACreatedUser;
+    private String AAUpdatedUser;
+    private String AACreatedDate;
+    private String AAUpdatedDate;
+    private String ARCustomerNo;
+    private String ARCustomerName;
+    private String ARCustomerGender;
+    private String ARCustomerBirthDay;
+    private String ARCustomerStatus;
+    private String ARCustomerDesc;
+    private String ARCustomerPicture;
+    private String ARCustomerIDNumber;
+
+}
